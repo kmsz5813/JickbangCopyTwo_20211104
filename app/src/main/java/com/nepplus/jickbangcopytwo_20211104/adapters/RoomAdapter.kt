a77e1a23d90a486bd5c53209033e42974f179c82
김민성 <kmsz5813@naver.com>
2021-11-04 15:05:29 +0900

    xml 끌어와서 -> row에 담아서 -> 내보내기  - 어댑터 뼈대작업 마무리

--- app/src/main/java/com/nepplus/jickbangcopytwo_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopytwo_20211104/adapters/RoomAdapter.kt
@@ -1,11 +1,31 @@
 package com.nepplus.jickbangcopytwo_20211104.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.jickbangcopytwo_20211104.R
 import com.nepplus.jickbangcopytwo_20211104.datas.RoomData
 
 class RoomAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>() {
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var temRow = convertView
+        if (temRow == null) {
+            temRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = temRow!!
+
+        return row
+
+    }
+
 }
