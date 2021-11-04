7832a5d0c0ebca72bf443066faf082cc5f6bcf5e
김민성 <kmsz5813@naver.com>
2021-11-04 15:16:27 +0900

    getView 실제 데이터 가공 작업  - 설명 문구만 간단히 첨부

--- app/src/main/java/com/nepplus/jickbangcopytwo_20211104/MainActivity.kt
+++ app/src/main/java/com/nepplus/jickbangcopytwo_20211104/MainActivity.kt
@@ -2,11 +2,14 @@ package com.nepplus.jickbangcopytwo_20211104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.jickbangcopytwo_20211104.adapters.RoomAdapter
 import com.nepplus.jickbangcopytwo_20211104.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRomms = ArrayList<RoomData>()
+    lateinit var mRoomAdapter: RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         mRomms.add(RoomData(25000,"경기도 고양시",17,"3번째 방입니다."))
         mRomms.add(RoomData(8300,"서울시 중구",-2,"4번째 방입니다."))
         mRomms.add(RoomData(195300,"서울시 송파구",20,"5번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRomms)
+        roomListView.adapter = mRoomAdapter
 
 
     }
